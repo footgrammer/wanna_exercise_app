ac565f11c594352ab4bfd389dc43c4d41933eecd
손진성 <felixson99@gmail.com>
2025-04-23 16:14:48 +0900

    [UI]  채팅방 하단바 구현  및 라우터 연결결 Fixes #15

--- lib/UI/pages/chat/chat_page.dart
+++ lib/UI/pages/chat/chat_page.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:wanna_exercise_app/UI/pages/chat/chat_room_page.dart';
 
-class ChatPage extends StatelessWidget{
+class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         title: Align(
           alignment: Alignment.centerLeft,
-          child: Text('채팅 목록',
-          style: TextStyle(
-            color: Color(0xFF1414b8),
-            fontWeight: FontWeight.bold,
-          ),
-          )),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemCount: 10,
-        itemBuilder: 
-        (context, index){
-          return ListTile(
-            leading: Icon(Icons.account_circle,
-            size: 40,
-            color: Color(0xFF1414b8),
+          child: Text(
+            '채팅 목록',
+            style: TextStyle(
+              color: Color(0xFF1414b8),
+              fontWeight: FontWeight.bold,
             ),
-            title: Text('축구차실분'),
-            subtitle: Text('마지막에 한말....'),
-            trailing: Text('2025.04.23'),
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (_) => //chatRoomPage()
-              //   ),
-              //   );
-            },
-          );
-        }),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(
+                Icons.account_circle,
+                size: 40,
+                color: Color(0xFF1414b8),
+              ),
+              title: Text('축구차실분'),
+              subtitle: Text('마지막에 한말....'),
+              trailing: Text('2025.04.23'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => ChatRoomPage()),
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
