d222160c2a32fd08b9ba8c94e5fef5ecacf796c9
MJ <gwang907@gmail.com>
2025-04-23 16:59:25 +0900

    feat: 로그인페이지 텍스트폼필드에서 사용하는 유효성검사 함수 구현

--- lib/UI/pages/login/widgets/pw_text_form_field.dart
+++ lib/UI/pages/login/widgets/pw_text_form_field.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:wanna_exercise_app/core/validator_login.dart';
 
 class PwTextFormField extends StatelessWidget {
   const PwTextFormField({
     super.key,
     required this.pwController,
     required this.focus,
+    required this.validator,
   });
 
   final TextEditingController pwController;
   final FocusNode focus;
-
+  final ValidatorLogin validator;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: pwController,
       focusNode: focus,
       textInputAction: TextInputAction.done,
+      validator: validator.validatorPw,
       onFieldSubmitted: (value) {
         // TODO: login 연결
         print('로그인 시도');
