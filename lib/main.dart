c13a05da301e6de5550fcc63d2b0ac65294ff4b8
footgrammer <junseok0211@nsu.ac.kr>
2025-04-23 12:23:38 +0900

    add : add folders

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Text('home'),
+      home: const Text('home page'),
     );
   }
 }
