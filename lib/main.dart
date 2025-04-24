b4f056b512e0c358c6b24bdffaf9c9c1eb126915
footgrammer <junseok0211@nsu.ac.kr>
2025-04-24 10:19:15 +0900

    refactor : change folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wanna_exercise_app/theme/light_theme.dart';
+import 'package:wanna_exercise_app/themes/light_theme.dart';
 import 'package:wanna_exercise_app/UI/pages/home/home_page.dart';
 
 void main() {
