a769f9670c7b1f232c0daaa30ada62ffade3287f
MJ <gwang907@gmail.com>
2025-04-24 10:31:13 +0900

    fix: 메인 pull 및 rebase

--- lib/model/model/user.dart
+++ lib/model/model/user.dart
@@ -1,36 +1,35 @@
 // 이메일
-// 전화번호
 // 비밀번호
 // 닉네임
 // 프로필 이미지
 
 class User {
   User({
+    required this.id,
     required this.email,
-    required this.phoneNumber,
     required this.pw,
     required this.nickname,
     required this.imgUrl,
   });
 
+  final String id;
   final String email;
-  final int phoneNumber;
   final String pw;
   final String nickname;
   final String imgUrl;
 
   // User.fromJson(Map<String, dynamic> json)
   //   : this(
+  //       id: json['id']
   //       email: json['email'],
-  //       phoneNumber: json['phoneNumber'],
   //       pw: json['pw'],
   //       nickname: json['nickname'],
   //       imgUrl: json['imgUrl'],
   //     );
 
   // Map<String, dynamic> toJson() => {
+  //   'id': id,
   //   'email': email,
-  //   'phoneNumber': phoneNumber,
   //   'pw': pw,
   //   'nickname': nickname,
   //   'imgUrl': imgUrl,
