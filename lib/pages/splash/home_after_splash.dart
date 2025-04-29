81044a7014c2860fb1a60518a65db447e9508212
MJ <gwang907@gmail.com>
2025-04-29 18:23:13 +0900

    fix: 앱 전체 로그인 상태관리 fix

--- lib/pages/splash/home_after_splash.dart
+++ lib/pages/splash/home_after_splash.dart
@@ -1,17 +1,23 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:wanna_exercise_app/pages/home/home_page.dart';
-import 'package:wanna_exercise_app/pages/login/login_page.dart';
 
-class HomeAfterSplash extends StatefulWidget {
-  final AsyncSnapshot<User?> snapshot;
-  const HomeAfterSplash({super.key, required this.snapshot});
+class HomeAfterSplash extends StatelessWidget {
+  final Widget child;
+  const HomeAfterSplash({required this.child});
 
   @override
-  State<HomeAfterSplash> createState() => HomeAfterSplashState();
+  Widget build(BuildContext context) {
+    return Stack(children: [child, SplashOverlay()]);
+  }
 }
 
-class HomeAfterSplashState extends State<HomeAfterSplash>
+class SplashOverlay extends StatefulWidget {
+  const SplashOverlay({super.key});
+
+  @override
+  State<SplashOverlay> createState() => SplashOverlayState();
+}
+
+class SplashOverlayState extends State<SplashOverlay>
     with SingleTickerProviderStateMixin {
   bool showSplash = true;
   late AnimationController controller;
@@ -25,11 +31,9 @@ class HomeAfterSplashState extends State<HomeAfterSplash>
       duration: const Duration(milliseconds: 400),
       vsync: this,
     );
-
     fadeAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(controller);
   }
 
-  // 3초 딜레이
   void startSplashTimer() async {
     await Future.delayed(const Duration(seconds: 3));
     if (mounted) {
@@ -48,25 +52,13 @@ class HomeAfterSplashState extends State<HomeAfterSplash>
 
   @override
   Widget build(BuildContext context) {
-    final user = widget.snapshot.data;
-    final Widget targetPage = user == null ? LoginPage() : HomePage();
-
-    return Stack(
-      children: [
-        // 메인 화면을 아래에 미리 로드해둠
-        targetPage,
-
-        if (showSplash)
-          FadeTransition(
-            opacity: fadeAnimation,
-            child: Scaffold(
-              backgroundColor: Color(0xFFE5E5E5),
-              body: Center(
-                child: Image.asset('assets/images/wanna_exercise.png'),
-              ),
-            ),
-          ),
-      ],
+    if (!showSplash) return const SizedBox.shrink();
+    return FadeTransition(
+      opacity: fadeAnimation,
+      child: Scaffold(
+        backgroundColor: const Color(0xFFE5E5E5),
+        body: Center(child: Image.asset('assets/images/wanna_exercise.png')),
+      ),
     );
   }
 }
